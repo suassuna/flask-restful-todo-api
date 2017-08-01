9da21472e521dec413544b71ecb8c1ffac76df46
suassuna <suassuna.paulo@gmail.com>
2017-08-01 17:43:36 -0300

    Added database connection.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Api
 from resources.todos import Todo, TodoList
+from db import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 
 api.add_resource(Todo, '/api/todos/<int:id>')
 api.add_resource(TodoList, '/api/todos')
 
+db.init_app(app)
 app.run(port=5000, debug=True)
